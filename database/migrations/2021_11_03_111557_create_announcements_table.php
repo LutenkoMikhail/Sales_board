78fa5153e7610665152c1302e30239adbc775f06
Lutenko Mikhail <lamprologus2011@gmail.com>
2021-11-03 13:52:46 +0200

    Add migrate and model

---
+++ database/migrations/2021_11_03_111557_create_announcements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50)->unique();
+            $table->text('content');
+            $table->float('price',12,2)->unsigned();
+            $table->timestamps();
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
