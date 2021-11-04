bf6aca83ce39f4dec18255cf1c9fa7b9b7e9e67f
Lutenko Mikhail <lamprologus2011@gmail.com>
2021-11-04 11:01:03 +0200

    Refactoring migration

--- database/migrations/2021_11_03_111557_create_announcements_table.php
+++ database/migrations/2021_11_03_111557_create_announcements_table.php
@@ -18,7 +18,19 @@ class CreateAnnouncementsTable extends Migration
             $table->string('title', 50)->unique();
             $table->text('content');
             $table->float('price',12,2)->unsigned();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->boolean('published')->default(false);
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')->onDelete('cascade');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')->onDelete('cascade');
+
             $table->index('created_at');
         });
     }
